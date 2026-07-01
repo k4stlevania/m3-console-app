d53391a9366a3ba66af52764741f758e2cb3f4fa
francisco <frncisco1243@gmail.com>
2026-07-01 16:35:51 -0400

    added the html structure and the js files

---
+++ js/libro.js
@@ -0,0 +1,42 @@
+// Un array para guardar los libros
+let books = [];
+
+// Crea un objeto de libro
+function crearLibro(title, author, pages, year, isbn, available = true){
+    return {
+        title,
+        autor,
+        pages,
+        year : Number(year),
+        isbn,
+        available
+    };
+}
+
+// anade un libro al array
+function addBook(book){
+    books.push(book);
+}
+// borra un libro del array
+function deleteBook(isbn){
+    books = books.filter((book) => book.isbn !== isbn);
+}
+// cambia la disponibilidad de un libro
+function changeAvailability(isbn){
+    const book = books.find((book) => book.isbn === isbn);
+    if(book){
+        book.available = !book.available;
+    }
+}
+//Busca todos los libros que pertenezcan a un autor
+function searchByAuthor(authorName){
+   let authorBooks = books.filter((book) => book.author == authorName)
+   return authorBooks;
+}
+// checkea si un libro ya existe por su codigo isbn
+function booksExist(isbn){
+    const bookExist = books.some((book) => String(isbn) === String(book.isbn));
+    if(bookExist){
+        throw new Error("Error ese libro ya existe en la libreria")
+    }
+}
